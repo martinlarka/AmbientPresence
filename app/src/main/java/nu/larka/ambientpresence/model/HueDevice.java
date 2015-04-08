6f446c3746ba28623d2bd387535387cd615e9d55
Martin Lärka <martin.larka@gmail.com>
2015-04-08 11:54:20 +0200

    Added hue device class

---
+++ app/src/main/java/nu/larka/ambientpresence/model/HueDevice.java
@@ -0,0 +1,10 @@
+package nu.larka.ambientpresence.model;
+
+/**
+ * Created by martin on 15-04-08.
+ */
+public class HueDevice extends Device {
+    public HueDevice(String deviceName) {
+        super(deviceName);
+    }
+}

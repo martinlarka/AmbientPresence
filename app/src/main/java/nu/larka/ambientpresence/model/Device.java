c7251c27300a3dd92532d54f4bd7879af4fe8177
Martin Lärka <martin.larka@gmail.com>
2015-04-08 11:54:10 +0200

    Added device super class

--- app/src/main/java/nu/larka/ambientpresence/model/Device.java
+++ app/src/main/java/nu/larka/ambientpresence/model/Device.java
@@ -4,4 +4,17 @@ package nu.larka.ambientpresence.model;
  * Created by martin on 15-04-08.
  */
 public class Device {
+    private String deviceName;
+
+    public Device(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
 }
